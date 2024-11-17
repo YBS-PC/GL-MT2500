bb3a17b8c9ad6f3392dacac100dab81ea1b9ac7d
YBS-GH <52429295+YBS-PC@users.noreply.github.com>
2024-11-18 00:08:30 +0300

    Update mt2500-part1.sh

--- sh/mt2500-part1.sh
+++ sh/mt2500-part1.sh
@@ -22,11 +22,11 @@
 # пакет luci-app-homeproxy находится внутри директории applications в двух ветках — master и openwrt-23.05.
 # Ветку openwrt-23.05 можно указать для совместимости с конкретной версией OpenWrt
 # а #luci-app-homeproxy в конце команды не имеет смысла, так как он используется для обозначения веток или тегов, а не директорий внутри ветки
-echo "src-git iwrt_packages https://github.com/immortalwrt/packages.git;openwrt-23.05" >> feeds.conf.default
-echo "src-git iwrt_luci https://github.com/immortalwrt/luci.git;openwrt-23.05" >> feeds.conf.default
+echo "src-git iwrt_packages https://github.com/immortalwrt/packages.git" >> feeds.conf.default
+echo "src-git iwrt_luci https://github.com/immortalwrt/luci.git" >> feeds.conf.default
 #
 #echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages#internet-detector luci-app-internet-detector luci-i18n-internet-detector-ru luci-app-log luci-i18n-log-ru" >> feeds.conf.default
 # internet-detector — это папка (директория) внутри основной ветки master, а не отдельная ветка или тег в репозитории.
 # В данном случае, нужно просто указать репозиторий без хэштега, так как хэштег используется для обозначения веток или тегов в репозиториях.
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
-echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openwrt" >> feeds.conf.default
+#echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages" >> feeds.conf.default
+#echo "src-git youtubeUnblock https://github.com/Waujito/youtubeUnblock.git;openwrt" >> feeds.conf.default
